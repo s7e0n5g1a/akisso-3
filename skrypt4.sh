3b1194d02afdd23e151f7d282ff0b8ab7f71cfb5
s7e0n5g1a <44317166+s7e0n5g1a@users.noreply.github.com>
2018-11-12 23:59:56 +0100

    Update skrypt4.sh

--- skrypt4.sh
+++ skrypt4.sh
@@ -6,13 +6,12 @@ echo $(lynx -dump $adres_strony) > strona1
 for i in `seq 1 100`
 do
 	echo $(lynx -dump $adres_strony) > strona2
-	zmiany=$(diff strona1 strona2) 
+	zmiany=$(diff -c strona1 strona2) 
        	if [ "$zmiany" != "" ]; then # jesli są różne
 		#zenity --info --title="Na stronie zostały dokonane zmiany!" --text="Zmiany: $zmiany " --width=400 --height=300
 		echo "Zostały dokonane zmiany:\n$zmiany"
 	fi
 	echo $(lynx -dump $adres_strony) > strona1
 	sleep $sekundy
-	
 done
 
