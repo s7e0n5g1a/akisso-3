5741fe377ad90b2317b86f19fa6ef1eddae2fd14
s7e0n5g1a <44317166+s7e0n5g1a@users.noreply.github.com>
2018-11-12 23:59:19 +0100

    Update skrypt3.sh

--- skrypt3.sh
+++ skrypt3.sh
@@ -4,5 +4,4 @@ curl -s http://api.icndb.com/jokes/random/ | python -c 'import sys, json; print 
 adres_url=`curl -s  --request GET   --url 'https://api.thecatapi.com/v1/images/search?format=json'   --header 'Content-Type: application/json'   --header 'x-api-key: 7fe06c0a-19d0-42c2-81fb-a1b20616c95b' | cut -d : -f 5- | tr -d '"}]'`
 
 wget $adres_url -O kotek
-img2txt kotek
-
+img2txt --width=100 __height=100 kotek
