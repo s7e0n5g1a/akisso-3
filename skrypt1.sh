346a11151f0e6306419d4d82c284de74e40abc65
s7e0n5g1a <44317166+s7e0n5g1a@users.noreply.github.com>
2018-11-12 23:58:21 +0100

    Update skrypt1.sh

--- skrypt1.sh
+++ skrypt1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-
-
+srednia_pobierania=0
+srednia_wysylania=0
 for i in `seq 1 100`
 do
 	pobieranie_1=`echo | grep -w -i wlp2s0 /proc/net/dev | tr -s ' '| cut -d " " -f 2`		
@@ -12,7 +11,35 @@ do
 	wysylanie_2=`echo | grep -w -i wlp2s0 /proc/net/dev | tr -s ' '| cut -d " " -f 10`
 	roznica_pobierania=$((pobieranie_2 - pobieranie_1))
 	roznica_wysylania=$((wysylanie_2 - wysylanie_1))
-	echo "Tempo pobierania: " | tr -d '\n'
+	srednia_pobierania=$((((srednia_pobierania*i)+roznica_pobierania)/(i+1)))
+	srednia_wysylania=$((((srednia_wysylania*i)+roznica_wysylania)/(i+1)))
+	echo "Srednia predkosc pobierania: " | tr -d '\n'
+	if [ $srednia_pobierania -lt 1000 ] ; then
+		echo "$srednia_pobierania" | tr -d '\n'
+		echo " B/s"
+	elif [ $srednia_pobierania -gt 1000000 ]  ; then
+		echo "$(($srednia_pobierania/1000000))" | tr -d '\n'
+		echo " MB/s"
+	else
+		echo "$(($srednia_pobierania/1000))" | tr -d '\n'
+		echo " KB/s"
+	fi
+
+
+	echo "Srednia predkosc wysylania: " | tr -d '\n'
+	if [ $srednia_wysylania -lt 1000 ] ; then
+		echo "$srednia_wysylania" | tr -d '\n'
+		echo " B/s"
+	elif [ $srednia_wysylania -gt 1000000 ]  ; then
+		echo "$(($srednia_wysylania/1000000))" | tr -d '\n'
+		echo " MB/s"
+	else
+		echo "$(($srednia_wysylania/1000))" | tr -d '\n'
+		echo " KB/s"
+	fi
+
+	
+	echo "Aktualna predkosc pobierania: " | tr -d '\n'
 	if [ $roznica_pobierania -lt 1000 ] ; then
 		echo "$roznica_pobierania" | tr -d '\n'
 		echo " B/s"
@@ -25,7 +52,7 @@ do
 	fi
 
 
-	echo "Tempo wysylania: " | tr -d '\n'
+	echo "Aktualna predkosc wysylania: " | tr -d '\n'
 	if [ $roznica_wysylania -lt 1000 ] ; then
 		echo "$roznica_wysylania" | tr -d '\n'
 		echo " B/s"
@@ -44,9 +71,17 @@ do
 	cat /sys/class/power_supply/BAT0/uevent | tail -n 5 | head -n 1 | cut -d "=" -f 2 | tr -d '\n'
 	echo "%"
 
-	echo "Obciążenie systemu wynosi: " | tr -d '\n'
+	echo "Srednie obciazenie dla ostatniej minuty: " | tr -d '\n'
 	cat /proc/loadavg | cut -d " " -f 1
+
+	echo "Srednie obciazenie dla ostatnich 5 minut: " | tr -d '\n'
+	cat /proc/loadavg | cut -d " " -f 2
+
+	echo "Liczba aktywnych procesow: " | tr -d '\n'
+	cat /proc/loadavg | cut -d " " -f 4
+
+	echo "ID najbardziej obciazajacego procesu: " | tr -d '\n'
+	cat /proc/loadavg | cut -d " " -f 5
 	echo
 
 done
-	
